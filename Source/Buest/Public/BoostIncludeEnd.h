64fc1954c7f34602a94b10ed50464b41a1d6b96e
David Morasz <david.morasz@meso.design>
2020-09-18 21:26:12 +0200

    ^ avoiding macro collisions for boost

--- Source/Buest/Public/BoostIncludeEnd.h
+++ Source/Buest/Public/BoostIncludeEnd.h
@@ -1,3 +1,21 @@
 
 #pragma warning( pop )
+
+// restore temporary macro undefs
+#ifdef BUEST_DO_CHECK
+#undef BUEST_DO_CHECK
+
+#pragma pop_macro("DO_CHECK")
+#pragma pop_macro("checkCode")
+#pragma pop_macro("check")
+#pragma pop_macro("checkf")
+#pragma pop_macro("verify")
+#pragma pop_macro("verifyf")
+#pragma pop_macro("unimplemented")
+#pragma pop_macro("ensure")
+
+#endif
+
 THIRD_PARTY_INCLUDES_END
+
+#undef BOOST_INCLUDE_REGION
--- Source/Buest/Public/BoostIncludeStart.h
+++ Source/Buest/Public/BoostIncludeStart.h
@@ -1,4 +1,34 @@
+#ifdef BOOST_INCLUDE_REGION
+#error Boost include regions cannot be nested, one region is already opened before
+#endif
+#define BOOST_INCLUDE_REGION 1
+
 THIRD_PARTY_INCLUDES_START
 
+// temporarily undefine simplistic UE4 macros boost might also use
+#if DO_CHECK
+#define BUEST_DO_CHECK 1
+
+#pragma push_macro("DO_CHECK")
+#pragma push_macro("checkCode")
+#pragma push_macro("check")
+#pragma push_macro("checkf")
+#pragma push_macro("verify")
+#pragma push_macro("verifyf")
+#pragma push_macro("unimplemented")
+#pragma push_macro("ensure")
+
+#undef DO_CHECK
+#undef checkCode
+#undef check
+#undef checkf
+#undef verify
+#undef verifyf
+#undef unimplemented
+#undef ensure
+
+#endif
+
+// disable extra warnings THIRD_PARTY_INCLUDES_START didn't disable yet
 #pragma warning( push )
-#pragma warning( disable : 4103 )
+#pragma warning( disable : 4103 )   // alignment changed after including header, may be due to missing #pragma pack(pop)
