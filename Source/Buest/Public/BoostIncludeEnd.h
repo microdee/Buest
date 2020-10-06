9ed228cfc4f7a3166138baf20111d0f9811a4272
David Morasz <david.morasz@meso.design>
2020-10-06 22:21:29 +0200

    ^ added more macro guards

--- Source/Buest/Public/BoostIncludeEnd.h
+++ Source/Buest/Public/BoostIncludeEnd.h
@@ -2,6 +2,34 @@
 #pragma warning( pop )
 
 // restore temporary macro undefs
+
+#pragma pop_macro("TEXT")
+#pragma pop_macro("CONSTEXPR")
+#pragma pop_macro("PI")
+#pragma pop_macro("dynamic_cast")
+#pragma pop_macro("VARARGS")
+#pragma pop_macro("CDECL")
+#pragma pop_macro("STDCALL")
+#pragma pop_macro("FORCEINLINE")
+#pragma pop_macro("FORCENOINLINE")
+#pragma pop_macro("ABSTRACT")
+#pragma pop_macro("LINE_TERMINATOR")
+#pragma pop_macro("LINE_TERMINATOR_ANSI")
+#pragma pop_macro("DLLEXPORT")
+#pragma pop_macro("DLLIMPORT")
+#pragma pop_macro("LIKELY")
+#pragma pop_macro("UNLIKELY")
+#pragma pop_macro("RESTRICT")
+#pragma pop_macro("MOBILE")
+#pragma pop_macro("CONSOLE")
+#pragma pop_macro("PLATFORM_WINDOWS")
+#pragma pop_macro("PLATFORM_COMPILER_CLANG")
+#pragma pop_macro("PLATFORM_APPLE")
+#pragma pop_macro("PLATFORM_MAC")
+#pragma pop_macro("PLATFORM_LINUX")
+#pragma pop_macro("PLATFORM_FREEBSD")
+#pragma pop_macro("PLATFORM_UNIX")
+
 #ifdef BUEST_DO_CHECK
 #undef BUEST_DO_CHECK
 
@@ -17,8 +45,7 @@
 #endif
 
 #ifdef BUEST_MSVC_GNUC_AVOIDANCE
-//#pragma pop_macro("__GNUC__")
-#define __GNUC__ BUEST_MSVC_GNUC_AVOIDANCE
+#pragma pop_macro("__GNUC__")
 #endif
 
 THIRD_PARTY_INCLUDES_END
--- Source/Buest/Public/BoostIncludeStart.h
+++ Source/Buest/Public/BoostIncludeStart.h
@@ -6,6 +6,61 @@
 THIRD_PARTY_INCLUDES_START
 
 // temporarily undefine simplistic UE4 macros boost might also use
+
+#pragma push_macro("TEXT")
+#pragma push_macro("CONSTEXPR")
+#pragma push_macro("PI")
+#pragma push_macro("dynamic_cast")
+#pragma push_macro("VARARGS")
+#pragma push_macro("CDECL")
+#pragma push_macro("STDCALL")
+#pragma push_macro("FORCEINLINE")
+#pragma push_macro("FORCENOINLINE")
+#pragma push_macro("ABSTRACT")
+#pragma push_macro("LINE_TERMINATOR")
+#pragma push_macro("LINE_TERMINATOR_ANSI")
+#pragma push_macro("DLLEXPORT")
+#pragma push_macro("DLLIMPORT")
+#pragma push_macro("LIKELY")
+#pragma push_macro("UNLIKELY")
+#pragma push_macro("RESTRICT")
+#pragma push_macro("MOBILE")
+#pragma push_macro("CONSOLE")
+#pragma push_macro("PLATFORM_WINDOWS")
+#pragma push_macro("PLATFORM_COMPILER_CLANG")
+#pragma push_macro("PLATFORM_APPLE")
+#pragma push_macro("PLATFORM_MAC")
+#pragma push_macro("PLATFORM_LINUX")
+#pragma push_macro("PLATFORM_FREEBSD")
+#pragma push_macro("PLATFORM_UNIX")
+
+#undef TEXT
+#undef CONSTEXPR
+#undef PI
+#undef dynamic_cast
+#undef VARARGS
+#undef CDECL
+#undef STDCALL
+#undef FORCEINLINE
+#undef FORCENOINLINE
+#undef ABSTRACT
+#undef LINE_TERMINATOR
+#undef LINE_TERMINATOR_ANSI
+#undef DLLEXPORT
+#undef DLLIMPORT
+#undef LIKELY
+#undef UNLIKELY
+#undef RESTRICT
+#undef MOBILE
+#undef CONSOLE
+#undef PLATFORM_WINDOWS
+#undef PLATFORM_COMPILER_CLANG
+#undef PLATFORM_APPLE
+#undef PLATFORM_MAC
+#undef PLATFORM_LINUX
+#undef PLATFORM_FREEBSD
+#undef PLATFORM_UNIX
+
 #if DO_CHECK
 #define BUEST_DO_CHECK 1
 
@@ -77,6 +132,7 @@ THIRD_PARTY_INCLUDES_START
 #if _MSC_VER > 0
 #pragma message ("BUEST: _MSC_VER - __GNUC__ macro collision detected, temporarily undefining __GNUC__ (GCC: " B_INCL_STR(__GNUC__) ", MSVC: " B_INCL_STR(_MSC_VER) ")")
 #define BUEST_MSVC_GNUC_AVOIDANCE __GNUC__
+#pragma push_macro("__GNUC__")
 #undef __GNUC__
 #endif
 
